e01849d95a498f27a5e2594f7b0be50fb1ce8cce
Akmal Hidayah <akmalhidayat826@gmail.com>
2026-05-22 18:25:32 +0800

    5/22/2026

--- resources/views/pengajuan-user/jsa/pdfjsa.blade.php
+++ resources/views/pengajuan-user/jsa/pdfjsa.blade.php
@@ -118,12 +118,12 @@
         }
 
         .company-title {
-            font-size: 12px;
+            font-size: 10.5px;
             font-weight: bold;
         }
 
         .document-title {
-            font-size: 11px;
+            font-size: 10px;
             font-weight: bold;
         }
 
@@ -171,9 +171,9 @@
         }
 
         .section-title {
-            margin: 9px 0 6px 0;
+            margin: 5px 0 4px 0;
             text-align: center;
-            font-size: 12px;
+            font-size: 10.5px;
             font-weight: bold;
         }
 
